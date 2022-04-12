d3822b47226998e0c5475fcda8eef436e9a1834a
leonardoademir <leoademir0@gmail.com>
2022-04-12 01:27:18 -0300

    removing unused fields

--- frontend/src/store/appContext.js
+++ frontend/src/store/appContext.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import getState from "./flux";
 
-// Don't change, here is where we initialize our context, by default it's just going to be null.
 export const Context = React.createContext(null);
 
 const injectContext = PassedComponent => {
 	const StoreWrapper = props => {
-		//this will be passed as the contenxt value
 		const [state, setState] = useState(
 			getState({
 				getStore: () => state.store,
@@ -20,19 +18,12 @@ const injectContext = PassedComponent => {
 		);
 
 		useEffect(() => {
-			/**
-			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
-			 * store, instead use actions, like this:
-			 **/
-			state.actions.getMessage(); // <---- calling this function from the flux.js actions
+
+			state.actions.getMessage(); 
             state.actions.syncTokenFromSessionStore();
 		}, []);
 
-		// The initial value for the context is not null anymore, but the current state of this component,
-		// the context will now have a getStore, getActions and setStore functions available, because they were declared
-		// on the state of this component
+
 		return (
 			<Context.Provider value={state}>
 				<PassedComponent {...props} />
--- frontend/src/store/flux.js
+++ frontend/src/store/flux.js
@@ -5,25 +5,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
             token: null,
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
             syncTokenFromSessionStore: () => {
                 const token = sessionStorage.getItem("token");
                 if(token && token != "" && token != undefined) {
@@ -58,28 +41,6 @@ const getState = ({ getStore, getActions, setStore }) => {
                 }
 
             },
-
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
